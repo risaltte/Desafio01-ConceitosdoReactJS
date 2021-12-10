99aa6976859fef7fd9dfb4826da21241e8331718
Rafael Risalte <risaltte@gmail.com>
2021-12-10 00:11:26 -0400

    start handleCreateNewTask function

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,13 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const newTask = {
+      id: 123456,
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
